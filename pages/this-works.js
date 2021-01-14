7fe6d10d3f237eb67f42389e568d1981ed829422
Simon Knott <info@simonknott.de>
2021-01-14 16:52:12 +0100

    Add reproduction stuff

--- pages/this-works.js
+++ pages/this-works.js
@@ -6,7 +6,8 @@ export function getServerSideProps() {
   };
 }
 
-export default function Home(props) {
+function Home(props) {
   return <div>hello world {props.date instanceof Date ? "true" : "false"}</div>;
 }
 
+export default Home;
